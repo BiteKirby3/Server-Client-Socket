834355af826ea1b4cbfd9af48bc502409f4612f5
Jingyuan Guan <jingyuan.guan@etu.utc.fr>
2021-04-03 20:09:43 +0200

    Update Server.java

--- src/serverPackage/Server.java
+++ src/serverPackage/Server.java
@@ -9,17 +9,22 @@ import java.net.Socket;
 import java.util.Hashtable;
 import java.util.Enumeration;
 
-public class Server {
 
+// La classe de serveur
+public class Server {
+	// Le num√©ro du port doit √™tre pareil que celui du client.
 	private static final int SERVER_PORT = 8000;
 	private ServerSocket listener;
 	private static Hashtable<String, ServerThread> clientThread = new Hashtable<String, ServerThread>();
+	// HashTable est pour stocker le nom d'utilisateur et son thread correspondant.
 
+	// constructeur de la classe, qui fait une connetion de Socket.
 	public Server() throws IOException {
 		this.listener = new ServerSocket(SERVER_PORT);
 		System.out.println("Server lanched.");
 	}
 
+	// fonction main qui comprend la boucle principale
 	public static void main(String[] args) throws IOException {
 		Server sc = new Server();
 		try {
@@ -37,9 +42,10 @@ public class Server {
 		return listener;
 	}
 
+	// La classe qui h√©rite Thread
 	private static class ServerThread extends Thread {
-		private String clientName;
-		private Socket client;
+		private String clientName; // nom du client en string
+		private Socket client; // le socket de chaque client
 		private PrintWriter out;
 		private BufferedReader in;
 
@@ -50,6 +56,7 @@ public class Server {
 		@Override
 		public void run() {
 			try {
+				// pour √©crire avant la boucle
 				out = new PrintWriter(client.getOutputStream(), true);
 				in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 				out.println("Bonjour, la connexion va s'arreter jusqu'au l'envoie d'un message 'exit'.");
@@ -58,31 +65,32 @@ public class Server {
 
 				int flag = 0;
 				String line = in.readLine();
-				while (!line.equals("exit")) {
-					if (flag == 0) {
-						while (clientThread.containsKey(line)) {
+				while (!line.equals("exit")) { // si le client n'entre pas 'exit'
+					if (flag == 0) { // pour son premier message, cad son pseudo
+						while (clientThread.containsKey(line)) { // lorsque son nom existe dans le hashtable
 							out.println("Nom existant. Veuillez reentrer votre pseudo : ");
 							line = in.readLine();
 						}
 						this.setClientName(line);
 						clientThread.put(this.getClientName(), this);
+						// imformer les autres client son entr√©e
 						System.out.println(this.getClientName() + " a rejoint la conversation.");
 						send(this.getClientName() + " a rejoint la conversation.");
-					} else {
+					} else { // a partir de son 2e message, on ajoute un pr√©fixe 'xxx a dit:'
 						System.out.println(this.getClientName() + " a dit : " + line);
 						send(this.getClientName() + " a dit : " + line);
 					}
 					flag++;
 					line = in.readLine();
-					if (line.equals("exit")) {
-						send(this.getClientName() + " a quitt®¶ la conversation.");
-						System.out.println(this.getClientName() + " a quitt®¶ la conversation.");
+					if (line.equals("exit")) { // le client a entr√©e 'exit', cad il veut quitter le chat
+						send(this.getClientName() + " a quitt la conversation.");
+						System.out.println(this.getClientName() + " a quitt la conversation.");
 					}
 				}
 				out.println("byeClient");
-			} catch (Exception e) {
-				System.out.println(this.getClientName() + " a quitt®¶ la conversation.");
-				send(this.getClientName() + " a quitt®¶ la conversation.");
+			} catch (Exception e) { // pour ce qui quitte le chat forc√©ment
+				System.out.println(this.getClientName() + " a quitt la conversation.");
+				send(this.getClientName() + " a quitt la conversation.");
 			} finally {
 				try {
 					in.close();
@@ -91,11 +99,11 @@ public class Server {
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-				clientThread.remove(this.getClientName());
+				clientThread.remove(this.getClientName()); // supprimer le client qui quitte
 			}
 		}
 
-		public void send(String s) {
+		public void send(String s) { // pour envoyer un message √† chaque client
 			Enumeration<String> e = clientThread.keys();
 			while (e.hasMoreElements()) {
 				String key = e.nextElement();
